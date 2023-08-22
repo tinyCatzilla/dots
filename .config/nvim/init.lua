1182dd7a90cd3a27708c646978cba611b8819049
tinyCatzilla <kary.ishwaran@gmail.com>
2023-08-21 20:56:43 -0400

    autochdir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,6 +24,7 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.scrolloff = 8
+vim.opt.autochdir = true
 
 -- -- Load custom tree-sitter grammar for org filetype
 -- require('orgmode').setup_ts_grammar()
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -36,7 +36,18 @@ local plugins = {
       require "custom.configs.copilot"
     end,
   },
-
+  {
+    "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("chatgpt").setup()
+      end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim"
+      }
+  },
   {
     'lervag/vimtex',
     lazy=false,
